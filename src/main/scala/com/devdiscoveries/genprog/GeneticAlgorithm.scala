9ea0e08d9408f4d5e24d18fde5abd3f0beddb136
Hugo Valk <hugo.valk@gmail.com>
2016-04-04 22:53:56 +0200

    Working on generating random program trees

--- src/main/scala/com/devdiscoveries/genprog/GeneticAlgorithm.scala
+++ src/main/scala/com/devdiscoveries/genprog/GeneticAlgorithm.scala
@@ -1,9 +1,32 @@
 package com.devdiscoveries.genprog
 
+import scala.util.Random
+
 trait GeneticAlgorithm[Individual] {
 
   def evolve
 
+}
+
+object Node {
+  def generateRandomTree[A](params: Seq[Param[A]],
+                            operations: Seq[(Node[A], Node[A]) => Operation[A]],
+                            probParams: Double = 0.1,
+                            probConst: Double = 0.4,
+                            probOperation: Double = 0.5,
+                            valueGenerator: () => A): Node[A] = {
+    require(probParams + probConst + probOperation == 1.0)
+    val random = Random.nextDouble()
+    if (random < probParams)
+      params(Random.nextInt(params.size))
+    else if (random < probParams + probConst)
+      Const(valueGenerator())
+    else {
+      val n1 = generateRandomTree(params, operations, probParams, probConst, probOperation, valueGenerator)
+      val n2 = generateRandomTree(params, operations, probParams, probConst, probOperation, valueGenerator)
+      operations(Random.nextInt(operations.size))(n1, n2)
+    }
+  }
 }
 
 sealed trait Node[A] {
@@ -18,14 +41,9 @@ case class Const[A](value: A) extends Node[A] {
   override def compute = value
 }
 
-case class Operation2[A](nodes: (Node[A], Node[A]), f: (A, A) => A)
+case class Operation[A](nodes: (Node[A], Node[A]), f: (A, A) => A)
     extends Node[A] {
   override def compute = f(nodes._1.compute, nodes._2.compute)
-}
-
-case class Operation3[A](nodes: (Node[A], Node[A], Node[A]), f: (A, A, A) => A)
-    extends Node[A] {
-  override def compute = f(nodes._1.compute, nodes._2.compute, nodes._3.compute)
 }
 
 trait GeneticProgrammingInterpreter extends GeneticAlgorithm[Node[Int]] {
--- src/test/scala/com/devdiscoveries/genprog/NodeSpec.scala
+++ src/test/scala/com/devdiscoveries/genprog/NodeSpec.scala
@@ -2,6 +2,7 @@ package com.devdiscoveries.genprog
 
 import org.scalatest.WordSpec
 import org.scalatest.MustMatchers
+import scala.util.Random
 
 class NodeSpec extends WordSpec with MustMatchers {
 
@@ -20,5 +21,12 @@ class NodeSpec extends WordSpec with MustMatchers {
       Param(4.1).compute mustEqual 4.1
     }
   }
+
+  val t = Node.generateRandomTree(params = List(Param(0)),
+    operations = List(add(_, _)),
+    valueGenerator = () => Random.nextInt)
+
+  def add(node1: Node[Int], node2: Node[Int]): Operation[Int] =
+    Operation[Int]((node1, node2), _ + _)
 
 }
